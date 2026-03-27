42650fe58231f44e25dcfaa254f20e03f6e99e24
Muhammad Faraz  Maqsood <faraz.maqsood@A006-01130.local>
2026-03-27 17:38:56 +0500

    chore: resolve pylint failure

--- openedx/core/djangoapps/user_api/accounts/views.py
+++ openedx/core/djangoapps/user_api/accounts/views.py
@@ -64,7 +64,11 @@ from openedx.core.djangoapps.user_api import accounts
 from openedx.core.djangoapps.user_api.accounts.image_helpers import get_profile_image_names, set_has_profile_image
 from openedx.core.djangoapps.user_api.accounts.utils import handle_retirement_cancellation
 from openedx.core.djangoapps.user_authn.exceptions import AuthFailedError
-from openedx.core.lib.api.authentication import BearerAuthentication, BearerAuthenticationAllowInactiveUser, OAuth2AuthenticationAllowInactiveUser
+from openedx.core.lib.api.authentication import (
+    BearerAuthentication,
+    BearerAuthenticationAllowInactiveUser,
+    OAuth2AuthenticationAllowInactiveUser
+)
 from openedx.core.lib.api.parsers import MergePatchParser
 
 from ..errors import AccountUpdateError, AccountValidationError, UserNotAuthorized, UserNotFound
@@ -1419,6 +1423,9 @@ class UsernameReplacementView(APIView):
 
 
 class OAuthUserInfoView(APIView):
+    """
+    API view for retrieving OAuth user information.
+    """
     authentication_classes = (
         OAuth2AuthenticationAllowInactiveUser,
         BearerAuthenticationAllowInactiveUser,
@@ -1428,6 +1435,14 @@ class OAuthUserInfoView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request):
+        """
+        Retrieve OAuth user information for the authenticated user.
+
+        Returns a JSON response containing the user's ID, email, first name, last name, and username.
+
+        :param request: The incoming request object.
+        :return: A JSON response containing the user's information.
+        """
         user = request.user
         if user and user.is_authenticated:
             log.info("OAuthUserInfoView user_id: %s", user.id)
